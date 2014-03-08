56156a15e99044e484188f4dbe77556b07eb783b
victorj589 <victorj589@gmail.com>
2014-03-07 21:12:34 -0800

    more layout work

---
+++ js/stack/zcript.js
@@ -0,0 +1,17 @@
+$(".towork").on("click", function(){
+$('html, body').animate({
+    scrollTop: $(".work").offset().top
+ }, 400);
+});
+
+$(".toabout").on("click", function(){
+$('html, body').animate({
+    scrollTop: $(".about").offset().top
+ }, 400);
+});
+
+$(".tocontact").on("click", function(){
+$('html, body').animate({
+    scrollTop: $(".contact").offset().top
+ }, 400);
+});
